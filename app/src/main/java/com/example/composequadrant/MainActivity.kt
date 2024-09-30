5c346b9194d0767b8716fc051a636d83ae86ae27
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 15:19:43 +0200

    add Modifier.weight(1f)

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +13,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -41,12 +46,44 @@ fun ComposeQuadrant() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Row() {
-
+        Row(Modifier.weight(1f)) {
+            Column(Modifier.weight(1f)) {
+                Quadrant()
+            }
+            Column(Modifier.weight(1f)) {
+                Quadrant()
+            }
         }
-        Row(){
-
+        Row(Modifier.weight(1f)){
+            Column(Modifier.weight(1f)) {
+                Quadrant()
+            }
+            Column(Modifier.weight(1f)) {
+                Quadrant()
+            }
         }
+    }
+}
+
+@Composable
+fun Quadrant(
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "title",
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "description",
+            textAlign = TextAlign.Justify
+        )
     }
 }
 
